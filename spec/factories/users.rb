b741885cf04b67863a092dd4e4ae5e175aa79ef9
Konke Gulwa <konkegulwa02@gmail.com>
2026-07-27 12:58:02 +0200

    Build authenticated CRM dashboard

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |number| "user#{number}@example.com" }
+    first_name { "Test" }
+    last_name { "User" }
+    password { "Password123!" }
+    password_confirmation { "Password123!" }
+    role { :member }
+
+    trait :admin do
+      role { :admin }
+    end
   end
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,10 +2,32 @@ require "rails_helper"
 
 RSpec.describe "Home", type: :request do
   describe "GET /" do
-    it "returns http success" do
-      get root_path
+    context "when the user is signed in" do
+      let(:user) { create(:user) }
 
-      expect(response).to have_http_status(:success)
+      before do
+        sign_in user
+      end
+
+      it "returns a successful response" do
+        get root_path
+
+        expect(response).to have_http_status(:success)
+      end
+
+      it "displays the user's first name" do
+        get root_path
+
+        expect(response.body).to include(user.first_name)
+      end
+    end
+
+    context "when the user is not signed in" do
+      it "redirects to the sign-in page" do
+        get root_path
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
