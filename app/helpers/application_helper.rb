a86e66db6fd3a21c0589fcf606f85183b2e08bd6
Konke Gulwa <konkegulwa02@gmail.com>
2026-08-03 13:21:39 +0200

    Add avatar initials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def avatar_initials(name)
+    return "?" if name.blank?
+
+    name
+      .split
+      .first(2)
+      .map { |part| part.first.upcase }
+      .join
+  end
 end
