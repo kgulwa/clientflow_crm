177cb6a416eed12561886841b8bf59e01d395888
Konke Gulwa <konkegulwa02@gmail.com>
2026-08-20 01:52:57 +0200

    Enable real email delivery

--- Gemfile
+++ Gemfile
@@ -80,7 +80,7 @@ group :development do
   gem "rubocop", require: false
   gem "rubocop-rails", require: false
   gem "rubocop-rspec", require: false
-  gem "letter_opener_web"
+
 
   # Performance
   # gem "rack-mini-profiler"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
   devise_for :users,
               controllers: {
                 registrations: "users/registrations"
